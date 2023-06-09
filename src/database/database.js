db94d3a93d2646ef088a1c6ad37135c6169bf5cb
Kevin <angelo,espinoza@espoch,edu,ec>
2023-06-09 15:30:03 -0500

    arreglos bd

--- src/database/database.js
+++ src/database/database.js
@@ -3,4 +3,5 @@ import  Sequelize  from "sequelize";
 export const sequelize = new Sequelize("peliculas", "postgres", "3919",{
     host: "127.0.0.1",
     dialect: "postgres",
+    port: "5454"
 });
